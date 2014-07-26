2feba8008b14dd61564e274ed82f0cfa764725ee
hgc <1209277239@qq.com>
2014-07-26 11:56:40 +0800

    sql_add1
    数据库添加数据

---
+++ categories.sql
@@ -0,0 +1,13 @@
+prompt Importing table CATEGORIES...
+set feedback off
+set define off
+insert into CATEGORIES (CategoryID, CategoryName, Picture)
+values ('1', '≥¥≤À', null);
+
+insert into CATEGORIES (CategoryID, CategoryName, Picture)
+values ('2', '¡π≤À', null);
+
+insert into CATEGORIES (CategoryID, CategoryName, Picture)
+values ('3', '≥¥»‚', null);
+
+prompt Done.
